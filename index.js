a3072bf9c328b1f1d4569653eca576493368b1cd
Rei <sovietspaceship@users.noreply.github.com>
2017-10-09 11:53:02 +0100

    pass options to ignore virtual fields

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 let _ = require('lodash');
 let fn = require('./lib/functions');
 
-let JoiSequelize = function(Sequelize, model) {
+let JoiSequelize = function(Sequelize, model, options = {}) {
   this._joi = {};
   this._model = model;
   this._types = {};
   this._allowNull = [];
+  this.omitVirtualFields = options.omitVirtualFields || true;
   this.sequelize = {
     define: require('./lib/define').bind(this),
-    fn: Sequelize.fn,
+    fn: Sequelize.fn.bind(Sequelize),
   };
   this.datatypes = require('./lib/datatypes');
   model(this.sequelize, this.datatypes);
